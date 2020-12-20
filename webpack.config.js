7e8dde5099a8ed4569191cad36174cfec297c074
Michael Wautier <wautiermichael@gmail.com>
2020-12-20 10:46:42 +0100

    html template

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,16 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
-    filename: "main.js",
+    filename: "main.[contenthash].js",
     path: path.resolve(__dirname, "dist")
   },
+  plugins: [new HtmlWebpackPlugin({
+    template: "./src/index.html"
+  })],
   module : {
     rules: [
       {
